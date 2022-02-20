2da57ca1db3858b15d962c3f8dc1106633ba1939
Plosie <7846678+Plosie@users.noreply.github.com>
2022-02-20 14:27:16 +0100

    Add files via upload

---
+++ trigger_autoscale.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+URL="$1"
+
+if [[ -z $1 ]]; then
+ echo "Syntax:"
+ echo "Please provide site to be stressed in the following fasion:" 
+ echo "./trigger_autoscale.sh 'www.somewebsite.tld:3000'"
+ exit 1
+fi
+
+for i in {1..1000}; do
+  curl $URL &
+  curl $URL &
+  curl $URL &
+  curl $URL &
+  curl $URL && sleep 0.2
+done
